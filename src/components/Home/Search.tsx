f4dfc63810f95a1784ed0a3809570a8128ab1e6a
Akeju Paul <akejupaul10@gmail.com>
2021-12-21 01:41:45 +0100

    search

---
+++ src/components/Home/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./css/Search.css"
+function Search() {
+  return (
+    <div >
+     <input className="input-container" type="search" name="" id="" ></input>
+    </div>
+  );
+}
+
+export default Search;
