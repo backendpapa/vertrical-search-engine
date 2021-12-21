a33a7773a68a4a8481804da38c02c6b23b64a163
Akeju Paul <akejupaul10@gmail.com>
2021-12-21 03:57:07 +0100

    added flexbox for the result component section

---
+++ src/components/Home/Results.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./css/Results.css"
+
+function Results() {
+  return (
+    <div className="container" >
+        <p>djdj</p>
+    </div>
+  );
+}
+
+export default Results;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Search from '../components/Home/Search';
+import Results from '../components/Home/Results';
 import "./css/Home.css"
 function Home() {
   return (
     <div className="flex-container" >
       <Search />
+      <Results />
     </div>
   );
 }
